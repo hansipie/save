ed10e729185b912751762ce4144a50807c771a72
Hansi P. <hansipie@users.noreply.github.com>
2024-09-27 18:42:41 +0300

    go public

---
+++ README.md
@@ -0,0 +1,51 @@
+# Save
+
+## Overview
+
+
+`save` is a "tee-like" utility to pipeline saving of content, while keeping the output stream intact.
+
+`SAVE_OUTPUT_PATH` environment variable needs to be set to define where you save.
+
+## Packaging
+
+Use pyinstaller to create a standalone executable.
+
+```bash
+$ pip install pyinstaller
+$ pyinstaller --onefile save.py
+```
+
+## Usage
+
+```bash
+Usage: save [--help] [--silent] [filename]
+
+save: a "tee-like" utility to pipeline saving of content, while keeping the output stream intact.
+
+positional arguments:
+  filename            filename to save the input. Use quotes if you have spaces. Resulting format is filename.txt by default
+
+options:
+  --ext            Custom file extension.
+  --silent        Don't use STDOUT for output, only save to the file.
+  --help          show this help message and exit.
+```
+
+## Examples
+
+```bash
+$ echo test | save output_file
+test
+
+$ cat {SAVE_OUTPUT_PATH}/output_file.txt
+test
+```
+
+```bash
+$ echo test | save --ext md output_file
+test
+
+$ cat {SAVE_OUTPUT_PATH}/output_file.md
+test
+```
---
+++ save.py
@@ -0,0 +1,45 @@
+import typer
+import sys
+import os
+
+from typing_extensions import Annotated
+
+
+def main(
+    filename: Annotated[str, typer.Argument(help="Filename without extension.")],
+    ext: Annotated[str, typer.Option(help="Custom file extension.")] = "txt",
+    silent: Annotated[
+        bool, typer.Option(help="Don't use STDOUT for output, only save to the file.")
+    ] = False,
+):
+    """
+    save: a "tee-like" utility to pipeline saving of content, while keeping the output stream intact.
+    """
+    path = os.environ.get("SAVE_OUTPUT_PATH")
+    if not path:
+        typer.secho(
+            "Error: Environment variable 'SAVE_OUTPUT_PATH' not set.", err=True
+        )
+        raise typer.Exit(1)
+    output = os.path.join(path, f"{filename}.{ext}")
+
+    with open(output, "w") as file:
+        try:
+            while True:
+                input_line = sys.stdin.readline()
+                if not input_line:
+                    break  # EOF
+                # write to file
+                file.write(input_line)
+                file.flush()
+                # write to stdout if not silent
+                if not silent:
+                    sys.stdout.write(input_line)
+                    sys.stdout.flush()  # Ensure the output is written immediately
+                
+        except KeyboardInterrupt:
+            pass  # Handle any cleanup here if necessary
+        
+
+if __name__ == "__main__":
+    typer.run(main)
